7d7aa4c360176bad155cfae643ab36d7205a3d7e
choi4624 <choi4624@gmail.com>
2023-03-09 22:11:42 +0900

    sql control + image view (example include)

--- src/main/java/server/image/image/Controller/UriController.java
+++ src/main/java/server/image/image/Controller/UriController.java
@@ -1,30 +1,27 @@
 package server.image.image.Controller;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.persistence.Id;
-
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
-import org.springframework.stereotype.Controller;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import server.image.image.Model.URI;
 import server.image.image.Repository.URIrepo;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class UriController{
@@ -56,32 +53,67 @@ public class UriController{
 
   }
 
-  @GetMapping("/view/{uid}")
-  public ResponseEntity<URI> imageViewUri(@PathVariable("uid") Long uid){
-    Optional<URI> uriData = URIrepo.findById(uid);
+  @GetMapping(value = "/viewsql/{uid}")
+  public Optional<URI> imageSql(@PathVariable("uid") Long uid){
+    Optional<URI> uriData = URIrepo.findByUid(uid);
 
     if(uriData.isPresent()){
-      return new ResponseEntity<>(uriData.get(),HttpStatus.OK);
+      return URIrepo.findByUid(uid);
     }
     else{
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      return Optional.empty();
     }
-  }
-
-  @GetMapping(value = "/viewsql/{uid}")
-  public Optional<URI> imageSql(@PathVariable("uid") Integer uid){
-    return URIrepo.findByUid(uid);
   }
 
   @GetMapping(value = "/basic")
   public Optional<URI> basicSql(){
     return URIrepo.basicSelect();
   }
+
+  @PostMapping(value = "/upload")
+  public ResponseEntity<String> uploadImage ( @RequestParam("filename") MultipartFile filename ){
+    
+    Date date = new Date();
+    StringBuilder sb = new StringBuilder();
+
+    if (filename.isEmpty()) {
+      sb.append("none");
+    } else {
+      sb.append(date.getTime());
+      sb.append(filename.getOriginalFilename());
+    }
+
+    if (!filename.isEmpty()) {
+      File dest = new File("image/src/main/storage/webupload/" + sb.toString());
+      try {
+        filename.transferTo(dest);
+      } catch (IllegalStateException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+    
+    return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);  
+    }
+    return new ResponseEntity<String>("ERROR in upload", HttpStatus.INTERNAL_SERVER_ERROR);
+
+  }
+  
+  @GetMapping(value = "view/{filename}.{extension}", produces = MediaType.IMAGE_PNG_VALUE)
+	public ResponseEntity<byte[]> viewEntity(@PathVariable("filename") String filename, @PathVariable("extension") String extension) throws IOException {
+		
+
+    InputStream imageStream = new FileInputStream("image/src/main/storage/webupload/" + filename + "." + extension);
+    byte[] imageByteArray = imageStream.readAllBytes();
+		imageStream.close();
+		return new ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK);
+	}
   /*
   @PostMapping("/view/{uid}")
 
   public URI getImageByUid(@PathVariable("uid") Long uid){
     final URI uri = URI.builder()
   }
-   */
+   */ 
+
 }
--- src/main/java/server/image/image/Repository/URIrepo.java
+++ src/main/java/server/image/image/Repository/URIrepo.java
@@ -4,28 +4,26 @@ package server.image.image.Repository;
 import org.springframework.data.jpa.repository.JpaRepository ;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import server.image.image.Model.URI;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Optional;
 
 @Repository
 public interface URIrepo extends JpaRepository <URI, Long> {
-    @Query(value = "Select uid, filename, reg_date from URI where uid in uid", nativeQuery = true)
-    Optional<URI> findByUid(Long uid);
+    @Query(value = "Select uid, filename, reg_date FROM URI WHERE UID = ?1", nativeQuery = true)
+    Optional<URI> findByUid(@Param("_uid") Long uid);
 
     @Query(value = "select uid from URI", nativeQuery = true)
     public Optional<URI> showTables();
 
-
     @Query(value = "select uid, filename, reg_date from URI", nativeQuery = true)
     public Optional<URI> selectAllData();
 
-    Optional<URI> findByUid(Integer uid);
 
-    @Query(value = "Select filename, UID, reg_date FROM URI WHERE UID = 100000", nativeQuery = true)
+    @Query(value = "Select uid, filename, reg_date FROM URI WHERE UID = 100000", nativeQuery = true)
     Optional<URI> basicSelect();
 }
